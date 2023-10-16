33311f448d2a9b18cdad2a773076a2b4a99d5430
Eliot Jordan <eliotj@princeton.edu>
2023-10-16 15:56:14 -0500

    Add bucket permissions

--- geoservices/geoservices_stack.py
+++ geoservices/geoservices_stack.py
@@ -1,19 +1,41 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_iam as iam,
+    aws_s3 as s3
 )
 from constructs import Construct
 
 class GeoservicesStack(Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, stage: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket_name = f"figgy-geo-{stage}"
+        bucket_arn = f"arn:aws:s3:::figgy-geo-{stage}"
+        bucket = s3.Bucket.from_bucket_arn(self, bucket_name, bucket_arn)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "GeoservicesQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        ## These must be set manually on bucket permissions page, unless creating
+        ## a new managed bucket. Settings here for reference
+        # public_access = s3.BlockPublicAccess(block_public_policy=False,
+        #                                     block_public_acls=False,
+        #                                     ignore_public_acls=False,
+        #                                     restrict_public_buckets=False)
+
+        permission = iam.PolicyStatement.from_json({
+            "Sid": "AllowCloudFrontServicePrincipal",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "cloudfront.amazonaws.com"
+            },
+            "Action": "s3:GetObject",
+            "Resource": f"arn:aws:s3:::figgy-geo-{stage}/*",
+            "Condition": {
+                "StringEquals": {
+                    "AWS:SourceArn": "arn:aws:cloudfront::080265008837:distribution/E31CT4TYD6ODK"
+                }
+            }
+        })
+
+        s3.CfnBucketPolicy(self, 'bucketpolicy',
+            bucket=bucket.bucket_name,
+            policy_document=iam.PolicyDocument(statements=[permission])
+        )
