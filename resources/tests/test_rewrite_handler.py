cfd8851cc8ef589af200cdb461c17d86fd42ad15
Eliot Jordan <eliotj@princeton.edu>
2023-11-09 14:04:46 -0600

    Update tests to work with newer versions of Python

--- resources/tests/test_rewrite_handler.py
+++ resources/tests/test_rewrite_handler.py
@@ -1,23 +1,19 @@
 import pytest
 import os
 import sys
+import json
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from urllib3_mock import Responses
+import unittest.mock as mock
 from rewrite_handler_production import handler as handler_production
 from rewrite_handler_staging import handler as handler_staging
 
-responses = Responses('urllib3')
-
-@responses.activate
 def test_rewrites_mosaic_id_production():
-    responses.add('GET', '/tilemetadata/banana',
-            body='{"uri": "s3://figgy-geo-production/ba/na/na/banana/mosaic-34.json"}',
-            status=200,
-            content_type='application/json')
-    event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/mosaicjson', 'querystring': 'id=banana'}}}]}
-    output = handler_production(event, {})
-    assert output['querystring'] == 'url=s3%3A%2F%2Ffiggy-geo-production%2Fba%2Fna%2Fna%2Fbanana%2Fmosaic-34.json&rescale=0%2C255'
+    http_mock = TestHelper().http_mock("s3://figgy-geo-production/ba/na/na/banana/mosaic-34.json")
+    with mock.patch('urllib3.PoolManager', return_value=http_mock):
+        event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/mosaicjson', 'querystring': 'id=banana'}}}]}
+        output = handler_production(event, {})
+        assert output['querystring'] == 'url=s3%3A%2F%2Ffiggy-geo-production%2Fba%2Fna%2Fna%2Fbanana%2Fmosaic-34.json&rescale=0%2C255'
 
 # Don't re-process URL if one is given.
 def test_keeps_existing_uri_production():
@@ -26,39 +22,40 @@ def test_keeps_existing_uri_production():
     output = handler_production(event, {})
     assert output['querystring'] == 'id=banana&url=test&rescale=0%2C255'
 
-# Don't re-process URL if one is given.
+# # Don't re-process URL if one is given.
 def test_keeps_existing_uri_staging():
     event = {'Records': [{'cf': {'request': {'uri':
       'https://test.com/mosaicjson', 'querystring': 'id=banana&url=test' }}}]}
     output = handler_staging(event, {})
     assert output['querystring'] == 'id=banana&url=test&rescale=0%2C255'
 
-@responses.activate
 def test_rewrites_cog_id_production():
-    responses.add('GET', '/tilemetadata/banana',
-            body='{"uri": "s3://figgy-geo-production/ba/na/na/banana/display_raster.tif"}',
-            status=200,
-            content_type='application/json')
-    event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/cog', 'querystring': 'id=banana'}}}]}
-    output = handler_production(event, {})
-    assert output['querystring'] == 'url=s3%3A%2F%2Ffiggy-geo-production%2Fba%2Fna%2Fna%2Fbanana%2Fdisplay_raster.tif&rescale=0%2C255'
+    http_mock = TestHelper().http_mock("s3://figgy-geo-production/ba/na/na/banana/display_raster.tif")
+    with mock.patch('urllib3.PoolManager', return_value=http_mock):
+        event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/cog', 'querystring': 'id=banana'}}}]}
+        output = handler_production(event, {})
+        assert output['querystring'] == 'url=s3%3A%2F%2Ffiggy-geo-production%2Fba%2Fna%2Fna%2Fbanana%2Fdisplay_raster.tif&rescale=0%2C255'
 
-@responses.activate
 def test_rewrites_mosaic_id_staging():
-    responses.add('GET', '/tilemetadata/banana',
-            body='{"uri": "s3://figgy-geo-staging/ba/na/na/banana/mosaic-34.json"}',
-            status=200,
-            content_type='application/json')
-    event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/mosaicjson', 'querystring': 'id=banana'}}}]}
-    output = handler_staging(event, {})
-    assert output['querystring'] == 'url=s3%3A%2F%2Ffiggy-geo-staging%2Fba%2Fna%2Fna%2Fbanana%2Fmosaic-34.json&rescale=0%2C255'
+    http_mock = TestHelper().http_mock("s3://figgy-geo-staging/ba/na/na/banana/mosaic-34.json")
+    with mock.patch('urllib3.PoolManager', return_value=http_mock):
+        event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/mosaicjson', 'querystring': 'id=banana'}}}]}
+        output = handler_staging(event, {})
+        assert output['querystring'] == 'url=s3%3A%2F%2Ffiggy-geo-staging%2Fba%2Fna%2Fna%2Fbanana%2Fmosaic-34.json&rescale=0%2C255'
 
-@responses.activate
 def test_rewrites_cog_id_staging():
-    responses.add('GET', '/tilemetadata/banana',
-            body='{"uri": "s3://figgy-geo-staging/ba/na/na/banana/display_raster.tif"}',
-            status=200,
-            content_type='application/json')
-    event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/cog', 'querystring': 'id=banana'}}}]}
-    output = handler_production(event, {})
-    assert output['querystring'] == 'url=s3%3A%2F%2Ffiggy-geo-staging%2Fba%2Fna%2Fna%2Fbanana%2Fdisplay_raster.tif&rescale=0%2C255'
+    http_mock = TestHelper().http_mock("s3://figgy-geo-staging/ba/na/na/banana/display_raster.tif")
+    with mock.patch('urllib3.PoolManager', return_value=http_mock):
+        event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/cog', 'querystring': 'id=banana'}}}]}
+        output = handler_production(event, {})
+        assert output['querystring'] == 'url=s3%3A%2F%2Ffiggy-geo-staging%2Fba%2Fna%2Fna%2Fbanana%2Fdisplay_raster.tif&rescale=0%2C255'
+
+class TestHelper:
+    def http_mock(self, uri):
+        json_string = json.dumps({"uri": uri})
+        response_mock = mock.MagicMock()
+        response_mock.status_code = 200
+        response_mock.data = bytes(json_string, 'utf-8')
+        http_mock = mock.MagicMock()
+        http_mock.request = mock.MagicMock(return_value=response_mock)
+        return http_mock
