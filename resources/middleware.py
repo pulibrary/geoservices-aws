7b9b57db33e8fcc84f8332e51b20383ccf464aa2
Eliot Jordan <eliotj@princeton.edu>
2023-11-29 16:45:01 -0600

    Add middleware to update TileJSON tile url values

--- resources/middleware.py
+++ resources/middleware.py
@@ -1,5 +1,5 @@
 import urllib3
-from starlette.datastructures import URL
+from starlette.datastructures import URL, QueryParams
 from starlette.requests import Request
 import json
 import os
@@ -61,3 +61,41 @@ class RewriteMiddleware:
             return f"s3://figgy-geo-production/{path}"
         else:
             return f"s3://figgy-geo-staging/{path}"
+
+class TileJSONMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        if scope["type"] != "http" or "tilejson" not in scope["path"]:
+            await self.app(scope, receive, send)
+            return
+        elif "method" in scope.keys() and scope["method"] != "GET":
+            await self.app(scope, receive, send)
+            return
+
+        async def update_tilejson(message):
+            if message["type"] == "http.response.body":
+                # convert json string to dict
+                obj = json.loads(message["body"].decode())
+
+                # get tiles url and extract the resource id
+                tile_url = URL(obj["tiles"][0])
+                query_params = QueryParams(tile_url.query)
+                url_param = query_params["url"]
+                resource_id = url_param.split("/")[-2]
+
+                # generate a new tile url with id in path and no url parameter
+                tile_url = tile_url.remove_query_params("url")
+                path = f"/{resource_id}{tile_url.path}"
+                tile_url = URL(scheme=tile_url.scheme, netloc=tile_url.netloc, path=path)
+
+                # insert tile url into json object and replace message body
+                obj["tiles"] = [f"{tile_url}"]
+                # raise Exception(message)
+                # import pdb; pdb.set_trace()
+                message["body"] = json.dumps(obj).encode()
+
+            await send(message)
+
+        await self.app(scope, receive, update_tilejson)
--- resources/tests/test_middleware.py
+++ resources/tests/test_middleware.py
@@ -3,7 +3,7 @@ import json
 
 from fastapi import FastAPI, Request
 from starlette.testclient import TestClient
-from middleware import HostMiddleware, RewriteMiddleware
+from middleware import HostMiddleware, RewriteMiddleware, TileJSONMiddleware
 
 def test_staging_middleware_for_mosaics_with_id_in_url(monkeypatch):
     monkeypatch.setenv('TITILER_BASE_URL', 'base_url')
@@ -16,6 +16,7 @@ def test_staging_middleware_for_mosaics_with_id_in_url(monkeypatch):
 
     app.add_middleware(HostMiddleware)
     app.add_middleware(RewriteMiddleware)
+    app.add_middleware(TileJSONMiddleware)
 
     with TestClient(app) as client:
         response = client.get('/123456/mosaicjson?stage=staging')
@@ -32,6 +33,7 @@ def test_production_middleware_for_mosaics_with_id_in_url(monkeypatch):
 
     app.add_middleware(HostMiddleware)
     app.add_middleware(RewriteMiddleware)
+    app.add_middleware(TileJSONMiddleware)
 
     with TestClient(app) as client:
         response = client.get('/123456/mosaicjson?stage=production')
@@ -48,6 +50,7 @@ def test_staging_middleware_for_cogs_with_id_in_url(monkeypatch):
 
     app.add_middleware(HostMiddleware)
     app.add_middleware(RewriteMiddleware)
+    app.add_middleware(TileJSONMiddleware)
 
     with TestClient(app) as client:
         response = client.get('/123456/cog?stage=staging')
@@ -64,7 +67,42 @@ def test_production_middleware_for_cogs_with_id_in_url(monkeypatch):
 
     app.add_middleware(HostMiddleware)
     app.add_middleware(RewriteMiddleware)
+    app.add_middleware(TileJSONMiddleware)
 
     with TestClient(app) as client:
         response = client.get('/123456/cog?stage=production')
         assert(json.loads(response.json())['url']) == 's3://figgy-geo-production/12/34/56/123456/display_raster.tif'
+
+def test_middleware_for_mosaic_tilejson(monkeypatch):
+    monkeypatch.setenv('TITILER_BASE_URL', 'base_url')
+    app = FastAPI()
+
+    @app.get("/mosaicjson/tilejson.json")
+    async def route1(req: Request):
+        tilejson = {"tilejson":"2.2.0","version":"1.0.0","scheme":"xyz","tiles":["https://tiles.prod/mosaicjson/tiles/WebMercatorQuad/{z}/{x}/{y}@1x?url=s3%3A%2F%2Fgeo-production%2F24%2F43%2F18%2F2443189116dd4a28bdd7da1384deb51e%2Fmosaic.json"],"minzoom":8,"maxzoom":13,"bounds":[67.50154032366855,9.00205452967639,94.49732854268473,31.999615061786308],"center":[80.99943443317665,20.50083479573135,8]}
+        return tilejson
+
+    app.add_middleware(HostMiddleware)
+    app.add_middleware(RewriteMiddleware)
+    app.add_middleware(TileJSONMiddleware)
+
+    with TestClient(app) as client:
+        response = client.get('/2443189116dd4a28bdd7da1384deb51e/mosaicjson/tilejson.json?stage=production')
+        assert(response.json()['tiles']) == ['https://tiles.prod/2443189116dd4a28bdd7da1384deb51e/mosaicjson/tiles/WebMercatorQuad/{z}/{x}/{y}@1x']
+
+def test_middleware_for_cog_tilejson(monkeypatch):
+    monkeypatch.setenv('TITILER_BASE_URL', 'base_url')
+    app = FastAPI()
+
+    @app.get("/cog/tilejson.json")
+    async def route1(req: Request):
+        tilejson = {"tilejson":"2.2.0","version":"1.0.0","scheme":"xyz","tiles":["https://tiles.prod/cog/tiles/WebMercatorQuad/{z}/{x}/{y}@1x?url=s3%3A%2F%2Fgeo-production%2F24%2F43%2F18%2F2443189116dd4a28bdd7da1384deb51e%2Fdisplay_raster.tif"],"minzoom":8,"maxzoom":13,"bounds":[67.50154032366855,9.00205452967639,94.49732854268473,31.999615061786308],"center":[80.99943443317665,20.50083479573135,8]}
+        return tilejson
+
+    app.add_middleware(HostMiddleware)
+    app.add_middleware(RewriteMiddleware)
+    app.add_middleware(TileJSONMiddleware)
+
+    with TestClient(app) as client:
+        response = client.get('/2443189116dd4a28bdd7da1384deb51e/cog/tilejson.json?stage=production')
+        assert(response.json()['tiles']) == ['https://tiles.prod/2443189116dd4a28bdd7da1384deb51e/cog/tiles/WebMercatorQuad/{z}/{x}/{y}@1x']
