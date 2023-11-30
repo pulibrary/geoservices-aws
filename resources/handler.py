b9f17ae95e7097f2fe2d70fffb280991436f175a
Eliot Jordan <eliotj@princeton.edu>
2023-11-30 11:41:32 -0600

    Remove compression middleware

--- resources/handler.py
+++ resources/handler.py
@@ -7,12 +7,12 @@ from mangum import Mangum
 from titiler.application.main import app
 from fastapi import Response
 from starlette.middleware.cors import CORSMiddleware
-from starlette_cramjam.middleware import CompressionMiddleware
 from titiler.core.middleware import LowerCaseQueryStringMiddleware
 
 # Reset middleware so we can apply our own settings
 app.user_middleware = []
 
+app.add_middleware(LowerCaseQueryStringMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -21,20 +21,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.add_middleware(
-    CompressionMiddleware,
-    minimum_size=0,
-    exclude_mediatype={
-        "image/jpeg",
-        "image/jpg",
-        "image/png",
-        "image/jp2",
-        "image/webp",
-        "application/json"
-    },
-)
-
-# Add middleware to set host header
 app.add_middleware(middleware.HostMiddleware)
 app.add_middleware(middleware.RewriteMiddleware)
 app.add_middleware(middleware.TileJSONMiddleware)
